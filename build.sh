576789f98da957a3fbaeaf613c59650772df1468
takashi <t.k.mk3.110@gmail.com>
2020-10-23 10:22:16 +0900

    upgrade to v2.2.1

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
 
 export GOOS=linux
 export GOARCH=amd64
-xcaddy build v2.2.0 \
-    --with github.com/traPtitech/caddy-trap-auth/v2@v2.2.0 \
+xcaddy build v2.2.1 \
+    --with github.com/traPtitech/caddy-trap-auth/v2@v2.2.1 \
     --with github.com/caddy-dns/lego-deprecated
