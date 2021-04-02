16f0dc384b644227822d12c84fbb43fd171563e5
sapphi-red <green@sapphi.red>
2021-04-02 21:58:35 +0900

    upgrade to v2.2.1+trap.1

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
 export GOOS=linux
 export GOARCH=amd64
 xcaddy build v2.2.1 \
-    --with github.com/traPtitech/caddy-trap-auth/v2@v2.2.1 \
+    --with github.com/traPtitech/caddy-trap-auth/v2@v2.2.1+trap.1 \
     --with github.com/caddy-dns/lego-deprecated
