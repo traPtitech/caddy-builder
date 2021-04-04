afd6f73d939d5e642a08dbfced0f8302cdadb5d7
sapphi-red <green@sapphi.red>
2021-04-04 19:11:50 +0900

    Update caddy to 1.0.5

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/abiosoft/caddy-git v0.0.0-20190703061829-f8cc2f20c9e7
-	github.com/caddyserver/caddy v1.0.1
+	github.com/caddyserver/caddy v1.0.5
 	github.com/captncraig/cors v0.0.0-20190703115713-e80254a89df1
 	github.com/nicolasazrak/caddy-cache v0.3.4
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
--- patch.sh
+++ patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 go mod download
-chmod -R +w "$GOPATH/pkg/mod/github.com/caddyserver/caddy@v1.0.1"
-patch "$GOPATH/pkg/mod/github.com/caddyserver/caddy@v1.0.1/caddyhttp/httpserver/plugin.go" < directive.patch
+chmod -R +w "$GOPATH/pkg/mod/github.com/caddyserver/caddy@v1.0.5"
+patch "$GOPATH/pkg/mod/github.com/caddyserver/caddy@v1.0.5/caddyhttp/httpserver/plugin.go" < directive.patch
