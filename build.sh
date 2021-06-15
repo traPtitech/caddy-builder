34ae6e71949f780b00504bfcd5906f534c3d57a7
hijiki51 <hibiki0719euph@gmail.com>
2021-06-16 00:57:52 +0900

    fix caddy-trap-auth version tag

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
 export GOOS=linux
 export GOARCH=amd64
 xcaddy build v2.4.2 \
-    --with github.com/traPtitech/caddy-trap-auth/v2@v2.4.2+trap.2 \
+    --with github.com/traPtitech/caddy-trap-auth/v2@v2.4.2 \
     --with github.com/caddy-dns/lego-deprecated
