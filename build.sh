d2f632e796a015910e780b5d56ebaa27233af96a
motoki317 <motoki317@gmail.com>
2023-05-16 13:51:59 +0900

    update to v2.6.4

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 
 export GOOS=linux
 export GOARCH=amd64
-xcaddy build "$CADDY_VERSION" \
+xcaddy build v2.6.4 \
     --with github.com/traPtitech/caddy-trap-auth/v2@v2.4.6 \
     --with github.com/caddy-dns/lego-deprecated
