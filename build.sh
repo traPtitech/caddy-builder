5d14574e9182ebf2fc6e8fa13e8d96e3d01ef1ab
motoki317 <motoki317@gmail.com>
2023-05-16 13:41:02 +0900

    read caddy version from env

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 
 export GOOS=linux
 export GOARCH=amd64
-xcaddy build v2.4.6 \
+xcaddy build "$CADDY_VERSION" \
     --with github.com/traPtitech/caddy-trap-auth/v2@v2.4.6 \
     --with github.com/caddy-dns/lego-deprecated
