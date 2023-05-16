697efaeefa2362e34cbe71f7abecd4b288b25bce
motoki317 <motoki317@gmail.com>
2023-05-16 20:11:28 +0900

    update plugin to 2.6.4

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 export GOOS=linux
 export GOARCH=amd64
 xcaddy build v2.6.4 \
-    --with github.com/traPtitech/caddy-trap-auth/v2@v2.4.6 \
+    --with github.com/traPtitech/caddy-trap-auth/v2@v2.6.4 \
     --with github.com/caddy-dns/lego-deprecated
